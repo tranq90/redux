ac5ecace4875ca2ad4c52d46f33ed9e24f2db990
Quan Tran <tran.q90@gmail.com>
2021-04-24 17:18:13 -0700

    useSelector and useDispatch with class based components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,18 @@
+import { Component } from "react";
 import classes from "./Counter.module.css";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, connect } from "react-redux";
 
+// FUNCTIONAL BASED
 const Counter = () => {
-  const counter = useSelector(state => state.counter);
+  const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({type: 'increment'})
+    dispatch({ type: "increment" });
   };
 
   const decrementHandler = () => {
-    dispatch({type: 'decrement'})
+    dispatch({ type: "decrement" });
   };
 
   const toggleCounterHandler = () => {};
@@ -29,3 +31,48 @@ const Counter = () => {
 };
 
 export default Counter;
+
+//CLASS BASED
+
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {
+
+//   }
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return{
+//     counter: state.counter
+//   }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({type: 'increment'}),
+//     decrement: () => dispatch({type: 'decrement'})
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
